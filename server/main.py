165ece9c20ce445c332cd29fcad240c2b99fb266
pkshah2017 <pkshah2017@gmail.com>
2021-03-06 19:16:54 -0500

    Read data from Arduino

--- server/main.py
+++ server/main.py
@@ -14,15 +14,22 @@ auth_token = os.environ['TWILIO_AUTH_TOKEN']
 to_phone_number = os.environ['PHONE_NUMBER']
 client = Client(account_sid, auth_token)
 
-
 # Serial Setup
-# ser = serial.Serial("/dev/ttyACM0", 9600)
+ser = serial.Serial("/dev/ttyUSB0", 9600)
 time.sleep(2)
 
 # Flask Setup
 app = Flask(__name__)
 CORS(app)
 
+# Data Points setup
+electricity = 0
+sound = 0
+gas = 0
+uv = 0
+temperature = 0
+pulse = 0
+fall_event = 0
 
 # Arduino Helpers
 def readFromArduino() -> str:
@@ -41,7 +48,15 @@ def getArduinoData() -> None:
     message = readFromArduino()
     if message and message != "" and message.replace(" ", "") != "":
         # Update globals / return data here
-        print(message)
+        data_points = message.split()
+        sound = message[0]
+        uv = message[1]
+        gas = message[2]
+        temperature = message[3]
+        electricity = message[4]
+        pulse = message[5]
+        fall_event = message[6]
+        print(data_points)
 
 
 def sendArduinoData() -> None:
@@ -66,13 +81,21 @@ def root() -> Dict[str, Any]:
     data = {"success": True}
     return data
 
+@app.route("/hud")
+def hud() -> Dict[str, Any]:
+    return {"electricity": electricity, "sound": sound, "gas": gas}
+
+# GET /hud
+# gas (int), sound (int), electricity (int 0-100)
+# Return {“electricity”: 10, “sound”: 73, “gas”: 940}
+
 
 # Main
 def main() -> None:
     # sendArduinoData()
-    # getArduinoData()
-    # app.run(host="0.0.0.0")
-    sendSMS("Execute Order 66")
+    getArduinoData()
+    app.run(host="0.0.0.0", port=5000)
+    # sendSMS("Execute Order 66")
 
 
 if __name__ == "__main__":
